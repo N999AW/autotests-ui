c35139389e5a5fb5c9eed236c47d3d92d3239680
N999AW <vlasovnikita2002@gmail.com>
2026-02-19 21:16:42 +0500

    Введение в pytest

---
+++ playwright_gl.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.firefox.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://re-store.ru/")
+    page.get_by_text("New")
+
+    page.wait_for_timeout(50000)
