3758f291fd1c679c98980aede24fdb30b123d41a
N999AW <vlasovnikita2002@gmail.com>
2026-03-30 15:31:35 +0500

    Знакомство с PageObject 2

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,31 +1,14 @@
 from playwright.sync_api import expect, Page, Playwright
 import pytest
+from pages.login_page import  LoginPage
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
 @pytest.mark.parametrize('email, password',
                          [('user.name@gmail.com', 'password'), ('user.name@gmail.com', '  '), ('  ', 'password')])
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    # Переходим на страницу авторизации
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-    # Находим поле "Email" и заполняем его
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    # Находим поле "Password" и заполняем его
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    # Находим кнопку "Login" и кликаем на нее
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    # Проверяем, что появилось сообщение об ошибке
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()  # Проверяем видимость элемента
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")  # Проверяем текст
-
-    # Пауза на 5 секунд, чтобы увидеть результат
-    chromium_page.wait_for_timeout(5000)
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
