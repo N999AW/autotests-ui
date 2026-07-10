21fda23e06fcac4675243bc47296a39ee9b8ab20
N999AW <vlasovnikita2002@gmail.com>
2026-07-11 03:01:06 +0500

    Логирование взаимодействий со страницей

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,6 +1,10 @@
-from xml.sax.xmlreader import Locator
+import allure
+from playwright.sync_api import Page, Locator, expect
 
-from playwright.sync_api import Page
+from tools.logger import get_logger  # Импортируем get_logger
+
+logger = get_logger("BASE_ELEMENT")  # Инициализируем logger
+
 
 class BaseElement:
     def __init__(self, page: Page, locator: str, name: str):
@@ -8,6 +12,38 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
-    def get_locator(self, **kwargs):
+    @property
+    def type_of(self) -> str:
+        return "base element"
+
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        return self.page.get_by_test_id(locator)
+        step = f'Getting locator with "data-testid={locator}" at index "{nth}"'
+
+        with allure.step(step):
+            logger.info(step)  # Добавили логирование
+            return self.page.get_by_test_id(locator).nth(nth)
+
+    def click(self, nth: int = 0, **kwargs):
+        step = f'Clicking {self.type_of} "{self.name}"'
+
+        with allure.step(step):
+            locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
+            locator.click()
+
+    def check_visible(self, nth: int = 0, **kwargs):
+        step = f'Checking that {self.type_of} "{self.name}" is visible'
+
+        with allure.step(step):
+            locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
+            expect(locator).to_be_visible()
+
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
+
+        with allure.step(step):
+            locator = self.get_locator(nth, **kwargs)
+            logger.info(step)  # Добавили логирование
+            expect(locator).to_have_text(text)
